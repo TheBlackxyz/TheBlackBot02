a0be6c7ff5d844cc80f16bd12429416fda7ab0d1
TheBlackXYZBotz <155623566+TheBlackxyz@users.noreply.github.com>
2024-09-05 22:01:21 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -17,12 +17,6 @@ from plugins.helper.admin_check import admin_fliter
 from urllib.parse import quote_plus
 from util.file_properties import get_name, get_hash, get_media_file_size
 from info import CHNL_LNK, GRP_LNK
-# image editor tools
-from image.edit_1 import bright, mix, black_white, g_blur, normal_blur, box_blur
-from image.edit_2 import circle_with_bg, circle_without_bg, sticker, edge_curved, contrast, sepia_mode, pencil, cartoon                             
-from image.edit_3 import green_border, blue_border, black_border, red_border
-from image.edit_4 import rotate_90, rotate_180, rotate_270, inverted, round_sticker, removebg_white, removebg_plain, removebg_sticker
-from image.edit_5 import normalglitch_1, normalglitch_2, normalglitch_3, normalglitch_4, normalglitch_5, scanlineglitch_1, scanlineglitch_2, scanlineglitch_3, scanlineglitch_4, scanlineglitch_5
 
 # configuration
 from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, PICS, IMDB, PM_IMDB, SINGLE_BUTTON, PROTECT_CONTENT, \
